472e5127fdd1ded6a838d32a57df5022206fcff6
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2021-06-03 15:22:53 -0300

    Add OpenFeign (Spring Cloud) to "hr-payroll" microservice

---
+++ hr-payroll-service/src/main/kotlin/br/com/hr/payroll/feign/EmployeeFeignClient.kt
@@ -0,0 +1,15 @@
+package br.com.hr.payroll.feign
+
+import br.com.hr.payroll.model.Employee
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
+@Component
+@FeignClient(name = "hr-employee-service", url = "http://localhost:8001", path = "/employees")
+interface EmployeeFeignClient {
+    @GetMapping("/{id}")
+    fun get(@PathVariable id: Long): ResponseEntity<Employee>
+}
--- hr-payroll-service/src/main/kotlin/br/com/hr/payroll/services/PaymentService.kt
+++ hr-payroll-service/src/main/kotlin/br/com/hr/payroll/services/PaymentService.kt
@@ -1,20 +1,13 @@
 package br.com.hr.payroll.services
 
-import br.com.hr.payroll.model.Employee
+import br.com.hr.payroll.feign.EmployeeFeignClient
 import br.com.hr.payroll.model.Payment
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
 
 @Service
-class PaymentService(val restTemplate: RestTemplate, @Value("\${hr-employee.host}") private val employeeHost: String) {
+class PaymentService(val employeeFeignClient: EmployeeFeignClient) {
     fun payment(employeeId: Long, days: Int): Payment {
-        var urlParams = HashMap<String, Long>()
-
-        urlParams["id"] = employeeId
-
-        val employee = restTemplate.getForObject("${employeeHost}/employees/{id}", Employee::class.java, urlParams)
-
-        return Payment(employee!!.name, employee.dailyIncome, days)
+        val employee = employeeFeignClient.get(employeeId).body
+        return Payment(employee!!.name, employee!!.dailyIncome, days)
     }
 }
