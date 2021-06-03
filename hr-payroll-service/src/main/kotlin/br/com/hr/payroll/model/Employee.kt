27d515c53e6ea9e95674fbb04ee84731fe09a410
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2021-06-03 14:58:04 -0300

    Get employee from microservice (PaymentService)

---
+++ hr-payroll-service/src/main/kotlin/br/com/hr/payroll/model/Employee.kt
@@ -0,0 +1,5 @@
+package br.com.hr.payroll.model
+
+data class Employee(val id: Long, val name: String, val dailyIncome: Double) {
+
+}
--- hr-payroll-service/src/main/kotlin/br/com/hr/payroll/services/PaymentService.kt
+++ hr-payroll-service/src/main/kotlin/br/com/hr/payroll/services/PaymentService.kt
@@ -1,11 +1,20 @@
 package br.com.hr.payroll.services
 
+import br.com.hr.payroll.model.Employee
 import br.com.hr.payroll.model.Payment
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
 
 @Service
-class PaymentService {
+class PaymentService(val restTemplate: RestTemplate, @Value("\${hr-employee.host}") private val employeeHost: String) {
     fun payment(employeeId: Long, days: Int): Payment {
-        return Payment("Test", 200.0, days)
+        var urlParams = HashMap<String, Long>()
+
+        urlParams["id"] = employeeId
+
+        val employee = restTemplate.getForObject("${employeeHost}/employees/{id}", Employee::class.java, urlParams)
+
+        return Payment(employee!!.name, employee.dailyIncome, days)
     }
 }
