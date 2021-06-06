529299efbfcc0ae309effe392d26f68c5f97c827
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2021-06-06 17:34:53 -0300

    Created Eureka Server project

---
+++ hr-eureka-server/src/test/kotlin/br/com/hr/eureka/HrEurekaServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.hr.eureka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
