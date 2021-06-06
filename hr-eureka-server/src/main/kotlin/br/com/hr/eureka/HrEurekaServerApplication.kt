c9bb7be01762fb28d70bf4f49cb6a5bfe42136ee
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2021-06-06 17:40:36 -0300

    Configure Eureka Server

--- hr-eureka-server/src/main/kotlin/br/com/hr/eureka/HrEurekaServerApplication.kt
+++ hr-eureka-server/src/main/kotlin/br/com/hr/eureka/HrEurekaServerApplication.kt
@@ -2,7 +2,9 @@ package br.com.hr.eureka
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+@EnableEurekaServer
 @SpringBootApplication
 class HrEurekaServerApplication
 
